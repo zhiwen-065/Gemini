4f450a5d6943777bcf27086e00eeb78e03b6e470
zhiwen-065 <jacobdawson6472@gmail.com>
2026-01-23 04:29:22 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles.css';
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+
+if (!el) {
+  document.body.innerHTML = '<div style="color:red;font-size:24px;padding:40px">NO #root FOUND</div>';
+} else {
+  createRoot(el).render(
+    <div style={{ color: 'white', padding: 40, fontSize: 20 }}>
+      <h1>ALGO BOOT OK</h1>
+      <p>If you can see this, JS is loading and React is mounted.</p>
+    </div>
+  );
+}
